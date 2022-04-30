2e3492c009f17feffc3a277be8fd7eb1825d72b3
Ugur <ugurkeklik.eem@gmail.com>
2022-04-30 19:21:30 +0100

    2nd HelloWorld deleted tab space

--- src/Practice/HelloWorld.java
+++ src/Practice/HelloWorld.java
@@ -3,7 +3,6 @@ package Practice;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-            System.out.println("2nd Hello World");
-
+        System.out.println("2nd Hello World");
     }
 }
