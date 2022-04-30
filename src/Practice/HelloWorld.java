74e2894bcd71c1aebeddd7c99e18bb5123e0cacc
Ugur <ugurkeklik.eem@gmail.com>
2022-04-30 14:47:53 +0100

    2nd HelloWorld added tab space

--- src/Practice/HelloWorld.java
+++ src/Practice/HelloWorld.java
@@ -3,6 +3,7 @@ package Practice;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("2nd Hello World");
+            System.out.println("2nd Hello World");
+
     }
 }
