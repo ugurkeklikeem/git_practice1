c2cce3b3ea5627769a7c3e2578eb33661a7e8dbc
Ugur <ugurkeklik.eem@gmail.com>
2022-04-30 19:58:44 +0100

    3rd line added

--- src/Practice/HelloWorld.java
+++ src/Practice/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("2nd Hello World");
+        System.out.println("3rd line");
     }
 }
